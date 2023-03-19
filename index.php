7fea291c6d96a787abe3f76409b9eca429a5275e
Raj Manna <raj2021manna@gmail.com>
2023-03-19 18:31:21 +0530

    third commit

--- index.php
+++ index.php
@@ -25,26 +25,17 @@
   </nav>
   <form action="index.php" method="post">
     <?php
-    $COMSCORE = 0;
-    $MYSCORE = 0;
-    ?>
-    <div class="container d-flex justify-content-between my-1">
-      <span class="text-start my-0">Computer Score: <?PHP echo $COMSCORE; ?></span>
-      <span class="text-end my-0">Your Score: <?php echo $MYSCORE; ?></span>
-    </div>
-    <?php
-    $oparr = array("oprock", "oppaper", "opscissors");
+    $oparr = array("oprock", "oppaper", "opscissors", "oprock", "oppaper", "opscissors");
     $generate_hand = array_rand($oparr, 1);
     ?>
-    <div class="container d-flex flex-column my-5">
+    <div class="container d-flex flex-column my-3">
       <div class="top mx-auto">
         <div class="name1 text-center d-flex flex-column">
-          <span class="text-bg-danger fw-bold py-1 h4 rounded rounded-2">Player 1</span>
-          <?php if($_SERVER['REQUEST_METHOD']=='POST'){
-            echo '<img src="src/image/<?php echo $oparr[$generate_hand]; ?>.png" alt="rock" class="mx-auto">';
-          }
-          else{
-            echo '<img src="src/image/<?php echo "hello"; ?>.png" alt="rock" class="mx-auto">';
+          <span class="text-bg-danger fw-bold py-1 px-2 h4 rounded rounded-2 my-2">Player 1 (Bot)</span>
+          <?php if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            echo '<img src="src/image/' . $oparr[$generate_hand] . '.png" alt="rock" class="mx-auto">';
+          } else {
+            echo '<img src="src/image/robo.png" alt="rock" class="mx-auto">';
           }
           ?>
         </div>
@@ -63,38 +54,70 @@
         if (isset($_POST['scissors'])) {
           $scissors = true;
         }
-      }
-      else{
+      } else {
         echo '<div class="bottom mx-auto my-4">
         <div class="name2 text-center d-flex flex-column">
-          <span class="text-bg-primary fw-bold py-1 h4 rounded rounded-2">Player 2</span>
-          <img src="src/image/rock.png" alt="rock" class="mx-auto">
+          <span class="text-bg-primary fw-bold py-1 px-2 h4 rounded rounded-2 my-2">Player 2 (You)</span>
+          <img src="src/image/person.png" alt="rock" class="mx-auto">
         </div>';
       }
       ?>
-    <?php
-    if($rock){
-      echo '<div class="bottom mx-auto my-4">
-        <div class="name2 text-center d-flex flex-column">
-          <span class="text-bg-primary fw-bold py-1 h4 rounded rounded-2">Player 2</span>
-          <img src="src/image/rock.png" alt="rock" class="mx-auto">
-        </div>'; }
-      if($paper){
+      <?php
+      if ($rock) {
         echo '<div class="bottom mx-auto my-4">
         <div class="name2 text-center d-flex flex-column">
-          <span class="text-bg-primary fw-bold py-1 h4 rounded rounded-2">Player 2</span>
+          <span class="text-bg-primary fw-bold py-1 px-2 h4 rounded rounded-2 my-2">Player 2 (You)</span>
+          <img src="src/image/rock.png" alt="rock" class="mx-auto">
+        </div>';
+      }
+      if ($paper) {
+        echo '<div class="bottom mx-auto my-4">
+        <div class="name2 text-center d-flex flex-column">
+          <span class="text-bg-primary fw-bold py-1 px-2 h4 rounded rounded-2 my-2">Player 2 (You)</span>
           <img src="src/image/paper.png" alt="rock" class="mx-auto">
         </div>';
       }
-      if($scissors){
+      if ($scissors) {
         echo '<div class="bottom mx-auto my-4">
         <div class="name2 text-center d-flex flex-column">
-          <span class="text-bg-primary fw-bold py-1 h4 rounded rounded-2">Player 2</span>
+          <span class="text-bg-primary fw-bold py-1 px-2 h4 rounded rounded-2 my-2">Player 2 (You)</span>
           <img src="src/image/scissors.png" alt="rock" class="mx-auto">
         </div>';
       }
+      ?>
+    </div>
+    </div>
+    <?php
+    $COMSCORE = 'Ready';
+    $MYSCORE = 'Ready';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      if ($oparr[$generate_hand] == "oprock" && isset($_POST['scissors'])) {
+        $COMSCORE = 'Win';
+        $MYSCORE = 'Lose';
+      } elseif ($oparr[$generate_hand] == "oprock" && isset($_POST['paper'])) {
+        $MYSCORE = 'Win';
+        $COMSCORE = 'Lose';
+      } elseif ($oparr[$generate_hand] == "oppaper" && isset($_POST['rock'])) {
+        $COMSCORE = 'Win';
+        $MYSCORE = 'Lose';
+      } elseif ($oparr[$generate_hand] == "oppaper" && isset($_POST['scissors'])) {
+        $MYSCORE = 'Win';
+        $COMSCORE = 'Lose';
+      } elseif ($oparr[$generate_hand] == "opscissors" && isset($_POST['paper'])) {
+        $COMSCORE = 'Win';
+        $MYSCORE = 'Lose';
+      } elseif ($oparr[$generate_hand] == "opscissors" && isset($_POST['rock'])) {
+        $MYSCORE = 'Win';
+        $COMSCORE = 'Lose';
+      } else {
+        $MYSCORE = 'Tie';
+        $COMSCORE = 'Tie';
+      }
+    }
     ?>
-      </div>
+    <div class="container d-flex justify-content-center my-2">
+      <span class="fw-bold px-2 my-0">Your Status: <?php echo $MYSCORE; ?></span>
+      <span class="fw-bold px-2 my-0">Computer Status: <?PHP echo $COMSCORE; ?></span>
     </div>
     <div class="buttons text-center container">
       <p class="text-center mx-auto h4 py-1 text-bg-secondary w-75">Your HandPad</p>
